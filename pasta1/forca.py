b44d61dc887c18c07ff3eadeca67ffb59cd97f48
renataeugenia <eugeniacomercial@gmail.com>
2022-08-25 15:32:03 -0300

    curso Python

--- pasta1/forca.py
+++ pasta1/forca.py
@@ -3,6 +3,27 @@ def jogar():
     print("Bem vindo ao jogo de Forca!!!")
     print("***********************************")
 
+    palavra_secreta = "marmelada"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_", "_", "_", "_"]
+
+    enforcou = False
+    acertou = False
+
+    print(letras_acertadas)
+
+    while not enforcou and not acertou:
+
+        chute = input("Qual Letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if chute.upper() == letra.upper():
+                letras_acertadas[index] = letra
+            index = index + 1
+
+        print(letras_acertadas)
+
 
     print("Fim do jogo!")
 
